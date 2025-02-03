9e402fcd6f395778204bfe3abb79354cfc8def4f
lucky4990 <ezekiellucky297@gmail.com>
2025-02-03 21:55:48 +0100

    Added tests for new user tracking functionality

--- tests/access_control_test.ts
+++ tests/access_control_test.ts
@@ -17,3 +17,30 @@ Clarinet.test({
     assertEquals(block.receipts[0].result.expectOk(), true);
   }
 });
+
+Clarinet.test({
+  name: "Can retrieve users with access to an entry",
+  async fn(chain: Chain, accounts: Map<string, Account>) {
+    const wallet_1 = accounts.get("wallet_1")!;
+    const wallet_2 = accounts.get("wallet_2")!;
+    
+    // Grant access
+    let block = chain.mineBlock([
+      Tx.contractCall("access-control", "grant-access",
+        [types.uint(0), types.principal(wallet_2.address), types.bool(true), types.bool(false)],
+        wallet_1.address
+      )
+    ]);
+    
+    // Get users
+    block = chain.mineBlock([
+      Tx.contractCall("access-control", "get-entry-users",
+        [types.uint(0)],
+        wallet_1.address
+      )
+    ]);
+    
+    const result = block.receipts[0].result.expectOk().expectSome();
+    assertEquals(result['users'].length, 1);
+  }
+});
